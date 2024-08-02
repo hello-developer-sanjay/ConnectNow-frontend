86aa7f9e88587c25fb5ee403db34cfafb17fa3ff
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-08-02 14:46:06 +0530

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,40 +1,24 @@
 import React, { useRef, useEffect } from 'react';
-import styled from 'styled-components';
 
-const VideoContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-`;
-
-const VideoElement = styled.video`
-  width: 300px;
-  height: 300px;
-  background-color: black;
-`;
-
-const Video = ({ localStream, remoteStream }) => {
-  const localVideoRef = useRef(null);
-  const remoteVideoRef = useRef(null);
+const Video = ({ stream, isLocal, muted, isVideoOff }) => {
+  const videoRef = useRef();
 
   useEffect(() => {
-    if (localVideoRef.current) {
-      localVideoRef.current.srcObject = localStream;
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream;
     }
-  }, [localStream]);
-
-  useEffect(() => {
-    if (remoteVideoRef.current) {
-      remoteVideoRef.current.srcObject = remoteStream;
-    }
-  }, [remoteStream]);
+  }, [stream]);
 
   return (
-    <VideoContainer>
-      <VideoElement ref={localVideoRef} autoPlay playsInline muted />
-      <VideoElement ref={remoteVideoRef} autoPlay playsInline />
-    </VideoContainer>
+    <div>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        muted={muted || isLocal}
+        style={{ display: isVideoOff ? 'none' : 'block', width: '100%' }}
+      />
+    </div>
   );
 };
 
