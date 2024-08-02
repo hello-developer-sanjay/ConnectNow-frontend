db1c3ba4943fda274e60a8135311641cbab56960
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-08-02 10:12:04 +0530

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,39 +3,36 @@ import styled from 'styled-components';
 
 const VideoContainer = styled.div`
   display: flex;
-  justify-content: space-around;
-  margin: 1rem 0;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 `;
 
 const VideoElement = styled.video`
-  width: 45%;
-  height: auto;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  background: #000;
+  width: 300px;
+  height: 300px;
+  background-color: black;
 `;
 
-const Video = ({ localStream, remoteStream, isMuted, isVideoOff }) => {
+const Video = ({ localStream, remoteStream }) => {
   const localVideoRef = useRef(null);
   const remoteVideoRef = useRef(null);
 
   useEffect(() => {
-    if (localVideoRef.current && localStream) {
+    if (localVideoRef.current) {
       localVideoRef.current.srcObject = localStream;
-      console.log('Local video stream set:', localStream);
     }
   }, [localStream]);
 
   useEffect(() => {
-    if (remoteVideoRef.current && remoteStream) {
+    if (remoteVideoRef.current) {
       remoteVideoRef.current.srcObject = remoteStream;
-      console.log('Remote video stream set:', remoteStream);
     }
   }, [remoteStream]);
 
   return (
     <VideoContainer>
-      <VideoElement ref={localVideoRef} autoPlay playsInline muted={isMuted} style={{ display: isVideoOff ? 'none' : 'block' }} />
+      <VideoElement ref={localVideoRef} autoPlay playsInline muted />
       <VideoElement ref={remoteVideoRef} autoPlay playsInline />
     </VideoContainer>
   );
