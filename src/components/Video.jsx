62985119c40df4405b90daaf4df0fcc13482daf4
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-03-14 09:22:44 +0530

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://connectnow-backend-24july.onrender.com');
 
 export default function VideoChat() {
     const localVideoRef = useRef(null);
